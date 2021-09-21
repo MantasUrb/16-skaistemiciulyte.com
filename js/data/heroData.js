e82e3d7213b935d59c2637ab95909c58d88cf541
Mantas Urbonas <mantasu11@gmail.com>
2021-09-21 18:31:23 +0300

    correct content data update #21

--- js/data/heroData.js
+++ js/data/heroData.js
@@ -1,63 +1,80 @@
 const heroData = [
     {  
-    path:'./img/content/test1.jpg',  
-    title:'',
-    about:'',
-    link:'#'
+    path:'./img/content/VU_Siauliu Akademija_Admission process_C.webp',  
+    title:'VILNIUS UNIVERSITY ŠIAULIAI ACADEMY',
+    client: 'Client: Agency 1323',
+    about:'Advertising campaign on admission process for Vilnius University Šiauliai Academy.',
+    link:'vilnius-university-siauliai-academy'
     },
     {  
-    path:'./img/content/test2.webp',  
-    title:'',
+    path:'./img/content/EuropeanMovementLithuania_ChernobylsLessonsToEurope.webp',  
+    title:"CHERNOBYL'S LESSONS TO EUROPE",
+    client: 'Client: European Movement Lithuania',
     about:'',
-    link:'#'
+    link:'chernobyl-lessons-to-europe'
     },
     {  
-    path:'./img/content/test1.jpg',  
-    title:'',
+    path:'./img/content/VU_Siauliu Akademija_Steps.webp',  
+    title:'VILNIUS UNIVERSITY ŠIAULIAI ACADEMY',
+    client: 'Client: Agency 1323',
     about:'',
-    link:'#'
+    link:'vilnius-university-siauliai-academy-2'
     },
     {  
-    path:'./img/content/test2.webp',  
-    title:'',
+    path:'./img/content/BreakingOutToFreedom.webp',  
+    title:'BREAKING OUT TO FREEDOM',
+    client: 'Work for @artistswithbelarus initiative.',
     about:'',
-    link:'#'
+    link:'freedom'
     },
     {  
-    path:'./img/content/test1.jpg',  
-    title:'',
+    path:'./img/content/Overthinking.webp',  
+    title:'OVERTHINKING',
+    client: 'Personal work',
     about:'',
-    link:'#'
+    link:'overthinking'
     },
     {  
-    path:'./img/content/test2.webp',  
-    title:'',
+    path:'./img/content/MusicAddiction.webp',  
+    title:'ADDICTION TO MUSIC',
+    client: 'Personal work',
     about:'',
-    link:'#'
+    link:'music-addiction'
     },
     {
-    path:'./img/content/test1.jpg',  
-    title:'',
+    path:'./img/content/grilis.webp',  
+    title:'SEB ARENA. SUMMERTIME CAMPAIGN',
+    client: 'Client: Agency 1323',
     about:'',
-    link:'#'
+    link:'seb-arena-1'
     },
     {  
-    path:'./img/content/test2.webp',  
-    title:'',
+    path:'./img/content/MissingHat.webp',  
+    title:'MISSING HAT',
+    client: 'Personal work',
     about:'',
-    link:'#'
+    link:'missing-hat'
     },
     {
-    path:'./img/content/test1.jpg',  
-    title:'',
+    path:'./img/content/TennisBag.webp',  
+    title:'SEB ARENA. SOCIAL MEDIA CAMPAIGN',
+    client: 'Client: Agency 1323',
     about:'',
-    link:'#'
+    link:'seb-arena-tennis-bag'
     },
     {  
-    path:'./img/content/test2.webp',  
-    title:'',
+    path:'./img/content/Stable&Wobbly.webp',  
+    title:'STABLE & WOBBLY',
+    client: 'Personal work',
     about:'',
-    link:'#'
+    link:'the-tap'
+    },
+    {
+    path:'./img/content/HopesOfTheLockdownEnding.webp',  
+    title:'HOPES OF THE LOCKDOWN ENDING',
+    client: 'Personal work',
+    about:'',
+    link:'hopes-of-the-lockdown-ending'
     }
 ];
 
