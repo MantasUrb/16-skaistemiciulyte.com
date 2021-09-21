29558076a8f7c8808fd836723e4d9ad1f0d3df3e
Mantas Urbonas <mantasu11@gmail.com>
2021-09-21 22:28:21 +0300

    hero content render on GitHutPages fixed

--- js/components/hero.js
+++ js/components/hero.js
@@ -1,22 +1,15 @@
 function heroImages(selector, data) {
-    
-    const DOM = document.querySelector(selector);
-    const host = "http://127.0.0.1:3000/";
 
+    const DOM = document.querySelector(selector);
     let HTML = "";
-    if (window.location.href == host + 'index.html' 
-        ||
-        window.location.href == host) {
-        for (let i = 0; i < data.length; i++) {
-        const info = data[i];
-        HTML += ` <div class="col-4 col-xl-6 col-sm-12 image">
-                      <img src="${info.path}" alt="${info.title}" />
-                  </div>`;
-      }
-      DOM.innerHTML += HTML;
-    } else {
-        return;
+    
+    for (let i = 0; i < data.length; i++) {
+      const info = data[i];
+      HTML += ` <div class="col-4 col-xl-6 col-sm-12 image">
+                    <img src="${info.path}" alt="${info.title}" />
+                </div>`;
     }
+    DOM.innerHTML += HTML;
 }
 
 export { heroImages };
