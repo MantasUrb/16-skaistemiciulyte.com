5d900468e1c558ad162abad4401a61cc49bb0b9a
Mantas Urbonas <mantasu11@gmail.com>
2021-12-21 09:28:33 +0200

    test commit

---
+++ resources/views/portfolio/seb-arena-1.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
+    <title>SEB Arena. Summertime | Skaiste Miciulyte</title>
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet" />
+  </head>
+
+  <body id="about-body">
+    <!-- HEADER START -->
+    <div class="about-header">
+      <div class="header-logo"><a href="./">skaiste miciulyte</a></div>
+      <nav class="header-nav">
+        <ul class="hidden-sm hidden-md">
+          <li><a href="./">Work</a></li>
+          <li><a href="./about">About</a></li>
+          <li><a href="https://www.instagram.com/skaiste.miciulyte/" target="_blank">Instagram</a></li>
+        </ul>
+        <div class="menu-btn about-menu-btn">
+          <div class="menu-btn__burger"></div>
+        </div>
+      </nav>
+    </div>
+    <div id="mobile-nav" class="container-fluid mobile-nav">
+      <ul>
+        <li><a href="./">Work</a></li>
+        <div class="whiteline"></div>
+        <li><a href="./about">About</a></li>
+        <div class="whiteline"></div>
+        <li><a href="https://www.instagram.com/skaiste.miciulyte/" target="_blank">Instagram</a></li>
+      </ul>
+    </div>
+    <!-- HEADER END -->
+
+  <!-- HERO START -->
+  <div class="container inner-pages-container container-seb-arena-1">
+      <div class="row about-hero">
+        <div class="title col-12"><p>SEB ARENA. SUMMER CAMPAIGN</p></div>
+        <div class="client col-12"><p>Client: Agency 1323</p></div>
+        <div class="col-12">
+          <img class="inner-page-img" src="./img/content/seb-arena-1-grilis.webp" alt="Grill illustration">
+          <img class="inner-page-img" src="./img/content/seb-arena-1-kemperis.webp" alt="Camper illustration">
+          <img class="inner-page-img" src="./img/content/seb-arena-1-sodas.webp" alt="Garden illustration">
+          <img class="inner-page-img" src="./img/content/seb-arena-1-baidares.webp" alt="Canoes illustration">
+        </div>
+        <div class="col-12">
+          <img class="small-inner-page-img-sebarena1" src="./img/content/seb-arena-1-grilis-with-labels.webp" alt="Grill illustration for client">
+          <img class="small-inner-page-img-sebarena1" src="./img/content/seb-arena-1-kemperis-with-labels.webp" alt="Camper illustration for client">
+          <img class="small-inner-page-img-sebarena1" src="./img/content/seb-arena-1-sodas-with-labels.webp" alt="Garden illustration for clients">
+          <img class="small-inner-page-img-sebarena1" src="./img/content/seb-arena-1-baidares-with-labels.webp" alt="Canoes illustration for client">
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid hero inner-pages-hero">
+      <div class="row hero-row"></div>
+    </div>
+  <!-- HERO END -->
+
+    <!-- FOOTER START -->
+    <div class="footer-container about-footer-container">
+      <div class="footer">
+        <div class="socials"></div>
+        <div class="email">
+          <a href="mailto:skaiste.miciulyte@gmail.com">skaiste.miciulyte@gmail.com</a>
+        </div>
+      </div>
+    </div>
+    <!-- FOOTER END -->
+    <script src="./js/home1Social.js" type="module" defer></script>
+  </body>
+</html>
